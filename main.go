b435ffba3e4e19e5b037d11874038935d1e94792
katexochen <katexochen@users.noreply.github.com>
2022-01-02 14:45:46 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+)
+
+func foo() error {
+	var multierr error
+	multierr = multierror.Append(multierr, ErrFirst)
+	multierr = multierror.Append(multierr, &SecondErr{code: 1})
+	multierr = multierror.Append(multierr, &SecondErr{code: 2})
+	multierr = multierror.Append(multierr, ErrThird)
+	multierr = multierror.Append(multierr, errors.New("other"))
+
+	return multierr
+}
+
+func bar() error {
+	var multierr error
+	multierr = multierror.Append(multierr, nil)
+	return multierr
+}
+
+func baz() error {
+	var multierr *multierror.Error
+	multierr = multierror.Append(multierr, nil)
+	return multierr.ErrorOrNil()
+}
+
+func lupz() (err error) {
+	var multierr *multierror.Error
+	defer returnErr(&multierr, &err)
+
+	multierr = multierror.Append(multierr, nil)
+	defer appendDeferredErr(&multierr, ErrFirst)
+
+	multierr = multierror.Append(multierr, ErrThird)
+	return
+}
+
+func appendDeferredErr(multiErr **multierror.Error, err error) {
+	*multiErr = multierror.Append(*multiErr, err)
+}
+
+func returnErr(err **multierror.Error, target *error) {
+	*target = (*err).ErrorOrNil()
+}
+
+var ErrFirst error = errors.New("first")
+
+type SecondErr struct {
+	code int
+}
+
+func (e *SecondErr) Error() string {
+	return fmt.Sprintf("second, code: %d", e.code)
+}
+
+var ErrThird error = errors.New("third")
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFoo(t *testing.T) {
+	assert := assert.New(t)
+	err := foo()
+	assert.Error(err)
+	assert.ErrorIs(err, ErrFirst)
+	assert.ErrorIs(err, ErrThird)
+	var secondErr *SecondErr
+	assert.ErrorAs(err, &secondErr)
+	assert.Equal(1, secondErr.code) // errors.As returns first error
+}
+
+func TestBar(t *testing.T) {
+	assert := assert.New(t)
+	err := bar()
+	assert.Error(err)
+	var multierr *multierror.Error
+	assert.ErrorAs(err, &multierr)
+}
+
+func TestBaz(t *testing.T) {
+	assert := assert.New(t)
+	err := baz()
+	assert.NoError(err)
+}
+
+func TestLupz(t *testing.T) {
+	assert := assert.New(t)
+	err := lupz()
+	assert.Error(err)
+	assert.ErrorIs(err, ErrFirst)
+	assert.ErrorIs(err, ErrThird)
+}
